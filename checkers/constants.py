cea6e76acb15c6f642facb57e6a42f0724c6488b
ash2620 <59735177+ash2620@users.noreply.github.com>
2020-12-07 18:08:20 +0530

    Add files via upload

---
+++ checkers/constants.py
@@ -0,0 +1,17 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//ROWS
+
+#RGB
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+
+CROWN = pygame.transform.scale(pygame.image.load(r'C:\Users\Aashish\PycharmProjects\Project\checkers\\assets\crown.png'), (45, 25))
+
